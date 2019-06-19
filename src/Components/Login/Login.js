bd1e4e4796ec8007dd7a47f4a6f1daa6ea3308e2
KrzGas <kgasik@gmail.com>
2019-06-19 15:43:36 +0200

    fixed login panel

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,34 +1,36 @@
 import React, { Component } from "react";
 
-class Login extends Component  {
+class Login extends Component {
   constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       hide: false
-    }
+    };
   }
 
   CloseLoginPanel = () => {
-    this.setState(prevState=>{
+    this.setState(prevState => {
       return {
         hide: !prevState.hide
-      }
-    })
-  }
+      };
+    });
+  };
 
   render() {
     return (
       <>
-      {this.state.hide ? null :
-      <div className="login__form">
-          <button>Zaloguj się</button>
-          <button>Zarejestruj się</button>
-          <span onClick={this.CloseLoginPanel}>X</span>
-      </div>
-      }
+        {this.state.hide ? null : (
+          <div className="login__curtain">
+            <div className="login__form">
+              <button>Zaloguj się</button>
+              <button>Zarejestruj się</button>
+              <span onClick={this.CloseLoginPanel}>X</span>
+            </div>
+          </div>
+        )}
       </>
     );
   }
-};
+}
 
 export { Login };
