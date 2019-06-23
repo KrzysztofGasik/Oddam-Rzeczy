f381bbdb399bab8753f7a448ed42c7130b35b6e6
KrzGas <kgasik@gmail.com>
2019-06-23 09:11:17 +0200

    walka z formularzem trwa

--- src/Components/Begin/Begin.js
+++ src/Components/Begin/Begin.js
@@ -28,8 +28,7 @@ const BgAfterLog = {
   backgroundImage: "url('../img/barrett-ward.jpg')",
   backgroundSize: "100% 100%",
   backgroundRepeat: 'no-repeat',
-  height: '100%',
-  margin: '0 0'
+  height: '100%'
 };
 
 const BeginBeforeLog = () => {
@@ -56,16 +55,39 @@ const BeginBeforeLog = () => {
 
 const BeginAfterLog = () => {
   return (
-    <section className="begin__wrapper" id="Begin"style={BgAfterLog}>
-      <span>Krok 1/4</span>
-      <form>
+    <section className="begin__wrapper__after" id="Begin"style={BgAfterLog}>
+      {/* <span>Krok 1/4</span>
+      <form id='step1'>
         <h1>Zaznacz co chcesz oddać:</h1>
         <label><input type="checkbox" name="ubrania" />ubrania, które nadają się do ponownego użycia</label>
         <label><input type="checkbox" name="ubrania2" />ubrania, do wyrzucenia</label>
         <label><input type="checkbox" name="zabawki" />zabawki</label>
         <label><input type="checkbox" name="ksiazki" />książki</label>
         <label><input type="checkbox" name="inne" />inne</label>
-        <button>Dalej</button>
+        <button id="next">Dalej</button>
+      </form> */}
+      <span>Krok 2/4</span>
+      <form id='step2'>
+        <label>Podaj liczbę 60l worków, w które zostaną spakowane rzeczy:
+        <select id="bags">
+          <option defaultValue="wybierz">-wybierz-</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+          <option value=">10">>10</option>
+        </select>
+        </label>	
+        <div className="begin__wrapper__button">
+        <button id="back">Wstecz</button>
+        <button id="next">Dalej</button>
+        </div>
       </form>
     </section>
   );
